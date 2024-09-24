e78cf2a4cbafd7b01409f7f99faccbd0705c4a24
oceansidez <2856016566@qq.com>
2024-09-24 16:42:38 +0800

    初始化项目

---
+++ LeetCode/src/main/java/com/study/Main.java
@@ -0,0 +1,11 @@
+package com.study;
+
+/**
+ * @author zzh
+ * @date 2024/9/24 16:27
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
